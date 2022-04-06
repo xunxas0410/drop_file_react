e1c0b9e78665988213cf31ceb092976349ae3cda
xanxus <ipk904904@gmail.com>
2022-04-06 22:58:59 +0800

    find 04/06 10:58

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
+import DropFileInput from './components/drop-file-input/DropFileInput';
+
 function App() {
+
+  const onFileChange = (files) => {
+
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">    
+      <h2 className="header">
+        React drop and files input 
+      </h2>
+    <DropFileInput
+    onFileChange={(files) => onFileChange(files)}
+     />
     </div>
   );
 }
